68f11644e3907e4406c7e79b68fdeb0db7f8c3ca
Ethans1428 <Ethans1228@gmail.com>
2026-03-09 13:28:35 +0000

    Created base WarehouseItem class

---
+++ src/model/WarehouseItem.java
@@ -0,0 +1,27 @@
+package model;
+
+public abstract class WarehouseItem {
+    private final String id;
+    private final String name;
+    private double price;
+
+    public WarehouseItem(String id, String name, double price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    // Class methods
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price=price;
+    }
+}
