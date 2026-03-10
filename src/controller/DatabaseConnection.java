e316d0c34f2d5252fe50c58aaea07c5f05c33430
Ethans1428 <Ethans1228@gmail.com>
2026-03-10 13:07:30 +0000

    Added DatabaseConnection.java to connect to the superbase database

---
+++ src/controller/DatabaseConnection.java
@@ -0,0 +1,22 @@
+package controller;
+import java.sql.*;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class DatabaseConnection {
+    private static String url, user, pass;
+
+    static {
+        Properties dbCredentials = new Properties();
+        try (FileInputStream fis = new FileInputStream(".env")) {
+            dbCredentials.load(fis);
+            url = dbCredentials.getProperty("DB_URL");
+            user = dbCredentials.getProperty("DB_USER");
+            pass = dbCredentials.getProperty("DB_PASSWORD");
+        } catch (Exception e) {
+            System.err.println("env file not found or incomplete"); }
+    }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, pass);
+    }
+}
